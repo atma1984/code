fc6b19603c0f2dba30fb31c6d72ce3084b40611d
atma1984 <vc_popov@mail.ru>
2025-08-18 17:55:40 +0300

    Chapter 12 AsyncWPF Example

---
+++ Chapter12/SynchronizingResourceAccess/Program.cs
@@ -0,0 +1,74 @@
+﻿using static System.Console;
+using System.Diagnostics;
+
+
+WriteLine("Please wait for the tasks to complete.");
+Stopwatch watch = Stopwatch.StartNew();
+Task a = Task.Factory.StartNew(MethodA);
+Task b = Task.Factory.StartNew(MethodB);
+Task.WaitAll(new Task[] { a, b });
+WriteLine();
+WriteLine($"Results: {SharedObjects.Message}.");
+WriteLine($"{watch.ElapsedMilliseconds:N0} elapsed milliseconds.");
+WriteLine($"{SharedObjects.Counter} string modifications.");
+
+
+
+static void MethodA()
+{
+    try
+    {
+        if (Monitor.TryEnter(SharedObjects.Conch, TimeSpan.FromSeconds(15)))
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                SharedObjects.Message += "A";
+                Interlocked.Increment(ref SharedObjects.Counter);
+                Write(".");
+            }
+        }
+        else
+        {
+            WriteLine("Method A timed out when entering a monitor on conch.");
+        }
+    }
+finally
+{
+        Monitor.Exit(SharedObjects.Conch);
+    }
+    //Thread.Sleep(SharedObjects.Random.Next(2000));
+                //try
+                //{
+                //    Monitor.Enter(SharedObjects.Conch);
+                //    for (int i = 0; i < 5; i++)
+                //    {
+                //        Thread.Sleep(SharedObjects.Random.Next(2000));
+                //        SharedObjects.Message += "A";
+                //        Write(".");
+                //    }
+                //}
+                //finally
+                //{
+                //    Monitor.Exit(SharedObjects.Conch);
+                //}
+            }
+static void MethodB()
+{
+    lock (SharedObjects.Conch)
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            Thread.Sleep(SharedObjects.Random.Next(2000));
+            SharedObjects.Message += "B";
+            Interlocked.Increment(ref SharedObjects.Counter);
+            Write(".");
+        }
+    }
+}
+static class SharedObjects
+{
+    public static object Conch = new();
+    public static Random Random = new();
+    public static string? Message; // общий ресурс
+    public static int Counter; // иной общий ресурс
+}
