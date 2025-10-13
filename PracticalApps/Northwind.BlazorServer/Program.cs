d1b757eb3d3c6a73d4410ce0be34fc5c1bbe339c
atma1984 <vc_popov@mail.ru>
2025-10-13 17:02:05 +0300

    Chapter Web API User Blazor Server not rendering

--- PracticalApps/Northwind.BlazorServer/Program.cs
+++ PracticalApps/Northwind.BlazorServer/Program.cs
@@ -11,6 +11,8 @@ builder.Services.AddSingleton<WeatherForecastService>();
 
 var app = builder.Build();
 
+Console.WriteLine("Application starting...");
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
