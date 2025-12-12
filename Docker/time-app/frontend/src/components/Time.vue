36b27ee6753aeda36d6f5c57b7e2391cc2588ac9
atma1984 <vc_popov@mail.ru>
2025-12-12 17:54:46 +0300

    Dockers environmant. docker-compose finish

--- Docker/time-app/frontend/src/components/Time.vue
+++ Docker/time-app/frontend/src/components/Time.vue
@@ -29,8 +29,8 @@ export default {
 <template>
   <div class="greetings">
     <h1 class="green">{{ currentTime }}</h1>
-    <h3>Нажмите кнопку для сохранения в базе данных</h3>
-    <button @click="saveTime">Сохранить время</button>
+    <h3>Нажмите кнопку для сохранения в базе данных mysql</h3>
+    <button @click="saveTime">Сохранить время сечас!</button>
     <h3 v-if="savedTimes.length && showSavedTimes">
       Ранее сохраненные времена:
     </h3>
--- Docker/time-app/frontend/vite.config.js
+++ Docker/time-app/frontend/vite.config.js
@@ -1,11 +1,17 @@
 import { fileURLToPath, URL } from 'url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { Server } from 'http'
+import { watch } from 'fs'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+   server: {
+    watch:{
+      usePolling: true
+    }
+   },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
